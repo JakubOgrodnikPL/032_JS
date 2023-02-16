0a10f86cf8b134f620ae50ecee3b56e23c77ef6d
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2023-02-16 08:44:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// const test = () => {
+// 	console.log('🍔')
+// }
+// test()
+// setTimeout(test,2000)
+// setInterval(test, 4000)
+
+// 'use strict'
+// const colors = ['red','green', 'blue']
+// for(let color of colors){
+// 	console.log(color)
+// }
+
+// alert('Cześć')
+
+// if (confirm('Chcesz 🍕?')){
+// 	console.log('ok');
+// } else{
+// 	console.log('nie to nie')
+// }
+
+// prompt('Cześć, jak masz na imię?')
+
+// console.log(Math.PI)
+
+// const liczbaLosowa = Math.random()*10
+// console.log(Math.floor(liczbaLosowa))
+
